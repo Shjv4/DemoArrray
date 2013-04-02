f4c9afa3901a92a9fd587e2456e7f7fe6ba896e8
hoangna <minhnl@Minhs-MacBook-2.local>
2013-04-02 14:45:26 +0700

    Initial Commit

---
+++ DemoArrray/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DemoArrray
+//
+//  Created by hoangna on 4/2/13.
+//  Copyright (c) 2013 HoangNA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
