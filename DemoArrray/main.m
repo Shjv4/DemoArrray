01d225fa364de90c34ac139e35bbbc88b222f991
hoangna <minhnl@Minhs-MacBook-2.local>
2013-04-02 16:18:42 +0700

    update example

--- DemoArrray/main.m
+++ DemoArrray/main.m
@@ -13,7 +13,7 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
+        // init array
         NSArray *listOfLetters1 = [NSArray arrayWithObjects:@"A", @"B", @"C", nil];
         NSLog(@"listOfLetters1 = %@", listOfLetters1);
         NSNumber *number1 = [NSNumber numberWithInt:1];
@@ -27,6 +27,30 @@ int main(int argc, const char * argv[])
         list[2] = @"F";
         NSMutableArray *listOfLetters2 = [[NSMutableArray alloc] initWithObjects:list count:3];
         NSLog(@"listOfLetters2 = %@, listOfLetters2.count = %lu", listOfLetters2, listOfLetters2.count);
+        // get element of array
+        NSString *stringObject1 = [listOfLetters1 objectAtIndex:0];
+        NSLog(@"stringObject1 = %@", stringObject1);
+        NSString *stringObject3 = [listOfLetters1 lastObject];
+        NSLog(@"stringObject3 = %@", stringObject3);
+        NSUInteger position = [listOfLetters1 indexOfObject:@"B"];
+        NSLog(@"index of string 'B' is %lu", position);
+        
+        // iterating through an array
+        for(NSMutableString *item in listOfLetters1) {
+            NSLog(@"This string in lower case is %@", [item lowercaseString]);
+        }
+        // demo makeObjectsPerformSelector
+        NSMutableString *string1 = [NSMutableString stringWithString:@"A"];
+        NSMutableString *string2 = [NSMutableString stringWithString:@"B"];
+        NSMutableString *string3 = [NSMutableString stringWithString:@"C"];
+        NSArray *listOfObjects = [NSArray arrayWithObjects:string1, string2, string3, nil];
+        NSArray *newArray = [[NSArray alloc] initWithArray:listOfObjects copyItems:YES];
+        [listOfObjects makeObjectsPerformSelector:@selector(appendString:) withObject:@"-MORE"];
+        NSLog(@"listOfObjects after append is %@", listOfObjects);
+        NSLog(@"newArray is %@", newArray);
+        [listOfObjects enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            NSLog(@"object(%lu)'s description is %@", idx, [obj description]);
+        }];
     }
     return 0;
 }
