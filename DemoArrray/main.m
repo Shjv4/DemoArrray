832c2e657f2887e0290e4adf49aa76b9fda13f35
hoangna <minhnl@Minhs-MacBook-2.local>
2013-04-02 15:05:56 +0700

    first commit

--- DemoArrray/main.m
+++ DemoArrray/main.m
@@ -14,8 +14,19 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
-        
+        NSArray *listOfLetters1 = [NSArray arrayWithObjects:@"A", @"B", @"C", nil];
+        NSLog(@"listOfLetters1 = %@", listOfLetters1);
+        NSNumber *number1 = [NSNumber numberWithInt:1];
+        NSNumber *number2 = [NSNumber numberWithInt:2];
+        NSNumber *number3 = [NSNumber numberWithInt:3];
+        NSMutableArray *listOfNumbers1 = [[NSMutableArray alloc] initWithObjects:number1, number2, number3, [NSNumber numberWithInt:5], nil];
+        NSLog(@"listOfNumbers1 = %@, listOfNumbers1.count = %lu", listOfNumbers1, listOfNumbers1.count);
+        id list[3];
+        list[0] = @"D";
+        list[1] = @"E";
+        list[2] = @"F";
+        NSMutableArray *listOfLetters2 = [[NSMutableArray alloc] initWithObjects:list count:3];
+        NSLog(@"listOfLetters2 = %@, listOfLetters2.count = %lu", listOfLetters2, listOfLetters2.count);
     }
     return 0;
 }
